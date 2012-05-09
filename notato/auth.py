316e6d6e151849cb403ccd46cabb2e288382becc
Kjetil Valle <kjetil.valle@bekk.no>
2012-05-09 15:43:24 +0200

    refactoring

--- notato/auth.py
+++ notato/auth.py
@@ -1,16 +1,13 @@
 from functools import wraps
-import flask
+from flask import Response, request
 import config
 
 def check_auth(username, password):
-    """This function is called to check if a username /
-    password combination is valid.
-    """
     return username == config.USERNAME and password == config.PASSWORD
 
 def authenticate():
     """Sends a 401 response that enables basic auth"""
-    return flask.Response(
+    return Response(
     'Could not verify your access level for that URL.\n'
     'You have to login with proper credentials', 401,
     {'WWW-Authenticate': 'Basic realm="Login Required"'})
@@ -18,7 +15,7 @@ def authenticate():
 def requires_auth(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-        auth = flask.request.authorization
+        auth = request.authorization
         if not auth or not check_auth(auth.username, auth.password):
             return authenticate()
         return f(*args, **kwargs)
