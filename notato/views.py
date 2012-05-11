0a0ec05c7fc14bb3699c2716b121ec4cd3335043
Kjetil Valle <kjetil.valle@bekk.no>
2012-05-11 13:09:36 +0200

    refactoring: renamed n -> note

--- notato/views.py
+++ notato/views.py
@@ -24,47 +24,47 @@ def notes():
 @app.route('/note/create/')
 @auth.requires_auth
 def create_note():
-    n = repo.Note(nid=repo.next_id())
-    return flask.render_template('edit_note.html', page_title='Create note', note=n)
+    note = repo.Note(nid=repo.next_id())
+    return flask.render_template('edit_note.html', page_title='Create note', note=note)
 
 @app.route('/note/read/<int:note_id>')
 @auth.requires_auth
 def read_note(note_id):
     if not repo.is_note(note_id):
         flask.abort(404)
-    n = repo.read(note_id)
-    if not n.title:
-        n.title = 'untitled note'
-    n.html = markdown.markdown(n.text)
-    return flask.render_template('read_note.html', note=n)
+    note = repo.read(note_id)
+    if not note.title:
+        note.title = 'untitled note'
+    note.html = markdown.markdown(note.text)
+    return flask.render_template('read_note.html', note=note)
 
 @app.route('/note/read/<int:note_id>.raw')
 @auth.requires_auth
 def read_note_raw(note_id):
     if not repo.is_note(note_id):
         flask.abort(404)
-    n = repo.read(note_id)
-    return flask.Response(n.text, 200, {'content-type': 'text/plain'})
+    note = repo.read(note_id)
+    return flask.Response(note.text, 200, {'content-type': 'text/plain'})
 
 @app.route('/note/edit/<int:note_id>', methods=['GET', 'POST'])
 @auth.requires_auth
 def edit_note(note_id):
     target = 'edit'
     if flask.request.method == 'POST':
-        note_title = flask.request.form.get('note_title', '').strip()
-        note_text = flask.request.form.get('note_text', '')
+        title = flask.request.form.get('note_title', '').strip()
+        text = flask.request.form.get('note_text', '')
         target = flask.request.form.get('target_state','edit')
-        n = repo.Note(note_id, note_title, note_text)
-        repo.write(n)
-        flask.flash('Note %d was successfully saved.' % note_id)
+        note = repo.Note(note_id, title, text)
+        repo.write(note)
+        flask.flash('Note was successfully saved.')
     else:
         if not repo.is_note(note_id):
             flask.abort(404)
-        n = repo.read(note_id)
+        note = repo.read(note_id)
     if target == 'read':
-        return flask.redirect(flask.url_for('read_note', note_id=n.id))
+        return flask.redirect(flask.url_for('read_note', note_id=note.id))
     else:
-        return flask.render_template('edit_note.html', page_title='Edit note', note=n)
+        return flask.render_template('edit_note.html', page_title='Edit note', note=note)
 
 @app.route('/note/delete/<int:note_id>')
 @auth.requires_auth
@@ -72,7 +72,7 @@ def delete_note(note_id):
     if not repo.is_note(note_id):
         flask.abort(404)
     repo.delete(note_id)
-    flask.flash('Note %d was successfully deleted.' % note_id)
+    flask.flash('Note was successfully deleted.')
     return flask.redirect(flask.url_for('index'))
 
 @app.route('/about/')
