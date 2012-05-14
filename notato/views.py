c9b9e685de6cd92645cd42aa3d28ecf256130049
Kjetil Valle <kjetil.valle@bekk.no>
2012-05-14 14:36:41 +0200

    custom 404 page

--- notato/views.py
+++ notato/views.py
@@ -102,3 +102,7 @@ def delete_note(note_id):
 def about():
     return flask.render_template('about.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return flask.render_template('404.html'), 404
+
