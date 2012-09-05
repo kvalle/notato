ee40b10c23cdab4e06f5720cc9b4572056eb5cf7
Kjetil Valle <kjetil.valle@bekk.no>
2012-09-05 21:40:36 +0200

    added login-test

--- notato/tests/test_login.py
+++ notato/tests/test_login.py
@@ -18,13 +18,15 @@ class NotatoTestCase(unittest.TestCase):
     def test_login_required(self):
         response = self.app.get('/', follow_redirects=True)
         assert '<h1>Log in</h1>' in response.data
+        
+    def test_login_works(self):
+        self.login()
+        response = self.app.get('/', follow_redirects=True)
+        assert 'Log out' in response.data
 
     def login(self):
         data = dict(username=self.username, password=self.password)
         return self.app.post('/log-in', data=data, follow_redirects=True)
-
-    def logout(self):
-        return self.app.get('/log-out', follow_redirects=True)
 
 if __name__ == '__main__':
     unittest.main()
