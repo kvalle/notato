4e3ad8ac8b9d351737f49f705def92729e113a44
Kjetil Valle <kjetil.valle@bekk.no>
2012-09-07 20:17:23 +0200

    note model tests

---
+++ notato/tests/test_models.py
@@ -0,0 +1,21 @@
+import unittest
+from notato.models import Note
+
+class NotatoTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.note = Note(1, text="## foo")
+
+    def test_markdown_note_content(self):
+        self.note.markdown = True
+        assert "<h2>foo</h2>" in self.note.content
+        
+    def test_plain_note_content(self):
+        self.note.markdown = False
+        assert "## foo" in self.note.content
+        
+if __name__ == '__main__':
+    unittest.main()
+    
+
+
