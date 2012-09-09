00c76d215ad012c497a12b072950281e1e56dd0a
Kjetil Valle <kjetil.valle@bekk.no>
2012-09-09 23:16:52 +0200

    added test for checking that login redirects back to original page

--- notato/tests/test_login.py
+++ notato/tests/test_login.py
@@ -1,5 +1,6 @@
 import unittest
 from notato.tests.base import NotatoTestCase
+from notato.models import Note
 
 class LoginTests(unittest.TestCase, NotatoTestCase):
 
@@ -14,6 +15,14 @@ class LoginTests(unittest.TestCase, NotatoTestCase):
         self.login()
         response = self.app.get('/')
         assert 'Log out' in response.data
+        
+    def test_login_redirects_properly(self):
+        self.repo.save(Note(10))
+        response = self.app.get('/notes/edit/10', follow_redirects=True)
+        assert "You must log in" in response.data
+        self.login()
+        response = self.app.get('/notes/edit/10')
+        assert "<h1>Edit note</h1>" in response.data
 
 if __name__ == '__main__':
     unittest.main()
