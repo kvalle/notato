f5da89910c065a143d2db5d987b4008bd9d2f837
Kjetil Valle <kjetil.valle@gmail.com>
2012-10-14 16:05:28 +0200

    updated pass

--- notato/config.py
+++ notato/config.py
@@ -1,5 +1,5 @@
-USERNAME = 'admin'
-PASSWORD = '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8'
+USERNAME = 'kjetil'
+PASSWORD = '24bd2b3885a9282c8f292344754955677b65ec6d'
 SECRET_KEY = 'dev_key'
 DEBUG = True
 DATABASE = 'notato'
