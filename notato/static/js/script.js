deeb536bf453df48a5f3cccfc70689c4fbcfe8ab
Kjetil Valle <kjetil.valle@gmail.com>
2012-12-20 21:34:08 +0100

    pimped file upload form

--- notato/static/js/script.js
+++ notato/static/js/script.js
@@ -3,3 +3,19 @@ $('.note-form .save-note').click(function(e){
     $('.note-form #target_state').val($(this).data('target-state'));
     $(this).parents('form:first').submit(); // Submit form
 });
+
+$('input[id=actual-file-field]').change(function() {
+	$('#fake-file-field').val($(this).val());
+});
+
+$(".new-file-button").click(function () {
+	$(".new-file-form").toggle("slow");
+}); 
+
+$("#fake-file-button").click(function () { 
+	$('input[id=actual-file-field]').click();
+});
+
+$("#fake-file-field").click(function () { 
+	$('input[id=actual-file-field]').click();
+}); 
